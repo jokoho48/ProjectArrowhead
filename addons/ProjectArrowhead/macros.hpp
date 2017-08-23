05fb9ea0d51ac896905d6fc5e8d5932b07a65984
jokoho48 <hoffmann.jonas95@gmail.com>
2017-08-23 19:10:04 +0200

    add More CFGPRAW values
    change some MX around

--- addons/ProjectArrowhead/macros.hpp
+++ addons/ProjectArrowhead/macros.hpp
@@ -25,15 +25,16 @@
 #define NOCLEAN(var) var setVariable [QCGVAR(noClean), true, true]
 
 #define MFUNC(var) EFUNC(Common,var)
-#define QMFUNC(var) QUOTE(MFUNC(var))
+#define QMFUNC(var) QEFUNC(Common,var)
 #define MGVAR(var) EGVAR(Common,var)
 #define QMGVAR(var) QUOTE(MGVAR(var))
-
 
 #define MISSIONCLASS missionConfigFile >> "ProjectArrowhead"
 
 #define CFGPRAW(var) QUOTE(PREFIX/var)
 #define CFGPRAW2(var1,var2) QUOTE(PREFIX/var1/var2)
+#define CFGPRAW3(var1,var2,var3) QUOTE(PREFIX/var1/var2/var3)
+#define CFGPRAW4(var1,var2,var3,var4) QUOTE(PREFIX/var1/var2/var3/var4)
 
 #define GETCLASS(var1,var2) ([var1, var2] call MFUNC(getClassName))
 #define NEXTSIDEMISSION call EFUNC(SideMission,selectSideMission);
