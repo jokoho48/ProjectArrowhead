eb5227551f85dc70aa22110c447a27a97b214614
jokoho48 <hoffmann.jonas95@gmail.com>
2017-12-27 01:14:25 +0100

    improve Object

--- addons/ProjectArrowhead/Compositions/collectIntel.hpp
+++ addons/ProjectArrowhead/Compositions/collectIntel.hpp
@@ -2,148 +2,148 @@ class CollectIntelCamp {
     alignOnSurface = 1;
     class item0 {
         classname = "Land_BagFence_Round_F";
-        offset[] = {-9.93091, -1.84717, 5.41931};
+        offset[] = {-9.97144, -1.85938, 5.41931};
         dirVector[] = {0.691097, 0.722762, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item1 {
         classname = "Land_BagFence_Round_F";
-        offset[] = {-10.0139, 0.475098, 5.41931};
+        offset[] = {-10.0544, 0.462891, 5.41931};
         dirVector[] = {0.75116, -0.66012, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item2 {
         classname = "Land_BagFence_Short_F";
-        offset[] = {-7.99487, -2.55127, 5.41428};
+        offset[] = {-8.0354, -2.56348, 5.41428};
         dirVector[] = {0.034776, 0.999395, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item3 {
         classname = "Land_BagFence_Short_F";
-        offset[] = {-8.05347, 1.13281, 5.41428};
+        offset[] = {-8.09399, 1.12061, 5.41428};
         dirVector[] = {-0.0188171, 0.999823, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item4 {
         classname = "Land_Sleeping_bag_brown_F";
-        offset[] = {-1.08154, 2.30908, 5.02582};
+        offset[] = {-1.12207, 2.29688, 5.02582};
         dirVector[] = {0.700486, -0.713666, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item5 {
         classname = "Land_Sleeping_bag_brown_F";
-        offset[] = {4.84619, -0.964355, 5.02582};
+        offset[] = {4.80566, -0.976563, 5.02582};
         dirVector[] = {-0.969711, 0.244254, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item6 {
         classname = "Land_Sleeping_bag_F";
-        offset[] = {1.5752, 3.24023, 5.02582};
+        offset[] = {1.53467, 3.22803, 5.02582};
         dirVector[] = {-0.0443869, -0.999014, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item7 {
         classname = "Land_Sleeping_bag_F";
-        offset[] = {4.02441, 1.95215, 5.02582};
+        offset[] = {3.98389, 1.93994, 5.02582};
         dirVector[] = {-0.769533, -0.638606, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item8 {
         classname = "Land_Sleeping_bag_F";
-        offset[] = {2.8208, -3.4126, 5.02582};
+        offset[] = {2.78027, -3.4248, 5.02582};
         dirVector[] = {-0.38492, 0.92295, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item9 {
         classname = "Land_Sleeping_bag_brown_F";
-        offset[] = {-1.97632, -0.171875, 5.02582};
+        offset[] = {-2.01685, -0.184082, 5.02582};
         dirVector[] = {0.999814, -0.0192851, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item10 {
         classname = "Land_Campfire_F";
-        offset[] = {1.37354, -0.244629, 5.21958};
+        offset[] = {1.33301, -0.256836, 5.21958};
         dirVector[] = {0.46051, 0.887655, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item11 {
         classname = "Land_WoodPile_F";
-        offset[] = {-0.0283203, -3.24854, 5.25993};
+        offset[] = {-0.0688477, -3.26074, 5.25993};
         dirVector[] = {0.86452, -0.502598, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item12 {
         classname = "Land_Ammobox_rounds_F";
-        offset[] = {-0.286621, 3.11475, 5.10536};
+        offset[] = {-0.327148, 3.10254, 5.10536};
         dirVector[] = {0.985786, 0.168007, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item13 {
         classname = "Land_Ammobox_rounds_F";
-        offset[] = {0.105957, 3.06787, 5.10536};
+        offset[] = {0.0654297, 3.05566, 5.10536};
         dirVector[] = {0.598521, 0.801107, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item14 {
         classname = "Land_CratesShabby_F";
-        offset[] = {11.033, 1.79053, 5.75984};
+        offset[] = {10.9924, 1.77832, 5.75984};
         dirVector[] = {-0.0281734, 0.999603, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item15 {
         classname = "Land_Sacks_heap_F";
-        offset[] = {9.51611, 1.85107, 5.39079};
+        offset[] = {9.47559, 1.83887, 5.39079};
         dirVector[] = {0.45612, 0.889918, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item16 {
         classname = "Land_CanisterPlastic_F";
-        offset[] = {-6.70068, -1.96729, 5.34782};
+        offset[] = {-6.74121, -1.97949, 5.34782};
         dirVector[] = {-0.506275, -0.862372, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item17 {
         classname = "Land_Axe_fire_F";
-        offset[] = {-0.634277, -2.25049, 5.02748};
+        offset[] = {-0.674805, -2.2627, 5.02748};
         dirVector[] = {0.101525, 0.994833, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item18 {
         classname = "Land_WoodenBox_F";
-        offset[] = {9.77368, -1.21191, 5};
+        offset[] = {9.73315, -1.22412, 5};
         dirVector[] = {-0.937825, 0.347109, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item19 {
         classname = "Land_WoodenBox_F";
-        offset[] = {11.0881, -0.208008, 5};
+        offset[] = {11.0476, -0.220215, 5};
         dirVector[] = {-0.940887, -0.338721, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
     class item20 {
-        classname = "Sign_Arrow_Yellow_F"; // "Intel_File2_F"
-        offset[] = {-9.4549, -0.864258, 5.00738};
-        dirVector[] = {0.803234, -0.595663, 0};
+        classname = "Sign_Arrow_Yellow_F";
+        offset[] = {-8.64514, -0.609863, 5.81742};
+        dirVector[] = {0, 1, 0};
         upVector[] = {0, 0, 1};
         TerrainSnap = 1;
     };
